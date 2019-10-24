1c5b487778e92e04d12be47eed5ee944ae1f74c4
GRAY JEANNE MEDINA (SHP-IRIS-ISD-OOCLL/MNL) <MEDINGR2@OOCL.COM>
2019-10-24 12:01:56 +0800

    added config file

---
+++ src/main/java/com/thoughtworks/miniPackage/configuration/HttpConfiguration.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.miniPackage.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class HttpConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .exposedHeaders("Content-disposition")
+                .allowCredentials(true)
+                .allowedMethods("PUT","POST","GET","DELETE","OPTIONS");
+    }
+}
